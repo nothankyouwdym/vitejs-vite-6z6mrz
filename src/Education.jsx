caa92f94ef15ac66c5696bdd1e2914308cf9d825
nothankyouwdym <boubacar.diallo2028@uppublicschools.org>
2022-07-06 15:01:26 -0400

    1 file modified

--- src/Education.jsx
+++ src/Education.jsx
@@ -13,12 +13,21 @@ const Education = () =>{
       <p>
         <strong>
           <em>
-            
+            In P.S.21 I was a good student with a majority of my teachers impressed by me. One of the main things I want to talk about if my 5th grade experiance it was all fun being taught by our teacher. But the grades and final report card is what shocked me. I got straight A's in my final grade.
           </em>
         </strong>
       </p>
 
       <h4>Middle School: University Prep Charter School</h4>
+
+
+      <p>
+        <strong>
+          <em>
+            In U Prep I was also a good student was good grades and I was just cruising. At the end my final gpa was 96.3 a good grade I was impressed to. Something else important was the fact I always made honor roll through all 4 quaters. Q1 I got Gold honor roll. Q2 I got Silver honor roll which is right below gold. In Q3 I got Gold honor roll. Finally in Q4 I got gold honor roll. The way the system works is Nothing to Silver to Gold being the highest. 
+          </em>
+        </strong>
+      </p>
     </section>
   )
 }
